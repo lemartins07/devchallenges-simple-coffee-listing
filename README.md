eda81855001b247bf76a0d4ed1e6078e0f600a77
lemartins07 <leandro.martins.85@gmail.com>
2023-12-08 10:08:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# devchallenges-simple-coffee-listing
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/lemartins07/devchallenges-simple-coffee-listing)
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,16 @@
+export const defaultTheme = {
+  white: '#FEF7EE',
+
+  'gray-100': '#1B1D1F',
+  'gray-300': '#111315',
+  'gray-400': '#6F757C',
+ 
+
+  'green-300': '#BEE3CC',
+  'yellow-500': '#F6C768',
+  'green-700': '#015F43',
+
+  'red-300': '#F75A68',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+} as const;
